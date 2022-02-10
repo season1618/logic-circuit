e7174ea0e5d0e5425641d2f7b8133ea03a114d31
season1618 <seasonminority314@gmail.com>
2022-02-10 18:52:13 +0900

    add align button EvenetListener

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import { canvas, ctx, cir } from './circuit.js';
 import './edit-circuit.js';
 
-// let alignButton = document.getElementById('align');
+let alignButton = document.getElementById('align');
 // let optimizeButton = document.getElementById('optimize');
 // let playButton = document.getElementById('play');
+
+alignButton.addEventListener(
+    'click',
+    function(){
+        cir.align();
+    }
+)
 
 cir.render();
