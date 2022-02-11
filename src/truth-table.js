eb93447ddce613a66049520777b9b48fa36dd6a7
season1618 <seasonminority314@gmail.com>
2022-02-11 22:00:04 +0900

    add/delete collumns

--- src/truth-table.js
+++ src/truth-table.js
@@ -18,12 +18,56 @@ class TruthTable {
 
         let thead = document.createElement('thead');
         let tbody = document.createElement('tbody');
-        
+
+        // thead
         thead.appendChild(document.createElement('tr'));
         thead.appendChild(document.createElement('tr'));
         thead.childNodes[0].appendChild(document.createElement('th'));
         thead.childNodes[0].childNodes[0].setAttribute('colspan', nInput + nOutput);
         thead.childNodes[0].childNodes[0].appendChild(document.createTextNode('Truth Table'));
+        let t = this;
+        thead.childNodes[1].addEventListener(
+            'mouseover',
+            function(e){
+                let width = e.target.getBoundingClientRect().right - e.target.getBoundingClientRect().left;
+                let celCount = e.offsetX / width
+                let posCel = celCount - Math.floor(celCount);
+                if(celCount < nInput){
+                    if(0.2 < posCel && posCel < 0.8){
+                        if(nInput > 1) this.style.cursor = 'zoom-out';
+                    }else{
+                        this.style.cursor = 'zoom-in';
+                    }
+                }else{
+                    if(0.2 < posCel && posCel < 0.8){
+                        if(nOutput > 1) this.style.cursor = 'zoom-out';
+                    }else{
+                        this.style.cursor = 'zoom-in';
+                    }
+                }
+            }
+        );
+        thead.childNodes[1].addEventListener(
+            'click',
+            function(e){
+                let width = e.target.getBoundingClientRect().right - e.target.getBoundingClientRect().left;
+                let celCount = e.offsetX / width
+                let posCel = celCount - Math.floor(celCount);
+                if(celCount < nInput){
+                    if(0.2 < posCel && posCel < 0.8){
+                        if(nInput > 1) t.setTable(nInput - 1, nOutput);
+                    }else{
+                        t.setTable(nInput + 1, nOutput);
+                    }
+                }else{
+                    if(0.2 < posCel && posCel < 0.8){
+                        if(nOutput > 1) t.setTable(nInput, nOutput - 1);
+                    }else{
+                        t.setTable(nInput, nOutput + 1);
+                    }
+                }
+            }
+        );
         for(let i = 0; i < this.nInput; i++){
             thead.childNodes[1].appendChild(document.createElement('th'));
             thead.childNodes[1].childNodes[i].appendChild(document.createTextNode(this.inputNames[i]));
@@ -33,12 +77,16 @@ class TruthTable {
             thead.childNodes[1].childNodes[i + this.nInput].appendChild(document.createTextNode(this.outputNames[i]));
         }
 
+        // tbody
         for(let i = 0; i < (1 << this.nInput); i++){
             tbody.appendChild(document.createElement('tr'));
+
+            // input
             for(let j = 0; j < this.nInput; j++){
                 tbody.childNodes[i].appendChild(document.createElement('td'));
                 tbody.childNodes[i].childNodes[j].appendChild(document.createTextNode((i >> nInput - 1 - j) & 1));
             }
+            // output
             for(let j = 0; j < this.nOutput; j++){
                 tbody.childNodes[i].appendChild(document.createElement('td'));
                 tbody.childNodes[i].childNodes[j + this.nInput].appendChild(document.createTextNode(0));
