14e03f627d769f685d11094d45a22dd775fc199d
season1618 <seasonminority314@gmail.com>
2022-02-11 19:19:59 +0900

    vary Node.include depending on logic gate, modify wiring in Circuit.render

--- src/circuit.js
+++ src/circuit.js
@@ -12,8 +12,20 @@ class Node {
         this.isVisited = false;
     }
     include(x, y){
-        if(this.x - scale < x && x < this.x && Math.abs(y - this.y) < 0.4*scale) return true;
-        else return false;
+        switch(this.kind){
+            case 'in':
+            case 'out':
+                if(Math.sqrt((x - this.x)**2 + (y - this.y)**2) < 0.4*scale) return true;
+                else return false;
+            case 'and':
+            case 'or':
+                if(this.x - scale < x && x < this.x && Math.abs(y - this.y) < 0.4*scale) return true;
+                else return false;
+            case 'not':
+                if(this.x - (0.7*1.73/2 + 0.16) * scale < x && x < this.x && Math.abs(y - this.y) < 0.35*scale) return true;
+                else return false;
+        }
+        
     }
     sort(l = 0, r = this.input.length){
         if(l + 1 >= r) return;
@@ -169,8 +181,22 @@ class Circuit extends Array {
                 ctx.beginPath();
                 ctx.moveTo(g.x, g.y);
                 ctx.lineTo(g.x * r + (this[i].x - scale) * (1 - r), g.y);
-                ctx.lineTo(g.x * r + (this[i].x - scale) * (1 - r), this[i].y - 0.4*scale + k/(n+1)*0.8*scale);
-                ctx.lineTo(this[i].x - scale, this[i].y - 0.4*scale + k/(n+1)*0.8*scale);
+                
+                switch(this[i].kind){
+                    case 'out':
+                        ctx.lineTo(g.x * r + (this[i].x - scale) * (1 - r), this[i].y);
+                        ctx.lineTo(this[i].x, this[i].y);
+                        break;
+                    case 'not':
+                        ctx.lineTo(g.x * r + (this[i].x - scale) * (1 - r), this[i].y - 0.35*scale + k/(n+1)*0.7*scale);
+                        ctx.lineTo(this[i].x - (0.7*1.73/2 + 0.16) * scale, this[i].y - 0.35*scale + k/(n+1)*0.7*scale);
+                        break;
+                    case 'and':
+                    case 'or':
+                        ctx.lineTo(g.x * r + (this[i].x - scale) * (1 - r), this[i].y - 0.4*scale + k/(n+1)*0.8*scale);
+                        ctx.lineTo(this[i].x - scale, this[i].y - 0.4*scale + k/(n+1)*0.8*scale);
+                        break;
+                }
                 ctx.stroke();
             }
         }
